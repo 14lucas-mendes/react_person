5d7bb138fdbd2af83328bf79c6ec142cef51f232
lucas mendes <luckajw@gmail.com>
2024-12-22 05:27:17 -0300

    ajuste na logica de para casado ou não casado e idade

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,13 +1,18 @@
+/* eslint-disable no-nested-ternary */
 export const Person = ({ person }) => (
   <section className="Person">
-    <h2 className="Person_name">{person.name};</h2>
-    <p className="Person_age">{person.age};</p>
+    <h2 className="Person_name">{person.name}</h2>
+    <p className="Person_age">{person.age ? person.age : undefined}</p>
     <p className="Person_sex">{person.sex}</p>
     <p className="Person_isMarried">
       {person.isMarried
-        ? person.sex === 'm'`${person.partner} is my wife`
+        ? person.sex === 'm'
+          ? `${person.partnerName} is my wife`
+          : `${person.partnerName} is my husband`
         : 'Not married'}
     </p>
-    <p className="Person_partner">{person.partner};</p>
+    {person.partnerName && (
+      <p className="Person_partner">Partner: {person.partnerName}</p>
+    )}
   </section>
 );
